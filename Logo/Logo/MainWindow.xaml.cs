d52466df2adb2f7d908b73e5e3f5a3195602ca37
Roman Shamin <roman@shamin.ru>
2020-04-14 09:05:14 +0300

    Add files via upload

---
+++ Logo/Logo/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Logo
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Logo = new TLogo(g);
+        }
+
+        TLogo Logo;
+
+        private void cmUp(object sender, RoutedEventArgs e)
+        {
+            Logo.Up(10);
+        }
+
+        private void cmDown(object sender, RoutedEventArgs e)
+        {
+            Logo.Up(-10);
+        }
+
+        private void cmRight(object sender, RoutedEventArgs e)
+        {
+            Logo.Right(10);
+        }
+
+        private void cmLeft(object sender, RoutedEventArgs e)
+        {
+            Logo.Right(-10);
+        }
+
+        private void cmJump(object sender, RoutedEventArgs e)
+        {
+            Logo.Jump();
+        }
+
+        private void cmHome(object sender, RoutedEventArgs e)
+        {
+            Logo.ToHome();
+        }
+    }
+}
---
+++ Logo/Logo/TLogo.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Logo
+{
+    class TLogo
+    {
+        Canvas g;
+
+        double X, Y;
+
+        public TLogo(Canvas g)
+        {
+            this.g = g;
+            X = g.Width / 2;
+            Y = g.Height / 2;
+
+            Draw();
+        }
+
+        Ellipse O;
+
+        double R = 10;
+
+        void Draw()
+        {
+            if (O == null)
+            {
+                O = new Ellipse();
+                O.Height = R;
+                O.Width = R;
+                O.Fill = Brushes.Red;
+                g.Children.Add(O);
+            }
+
+            if (X < 0)
+            {
+                X = 0;
+            }
+
+            if (X > g.Width)
+            {
+                X = g.Width;
+            }
+
+            if (Y < 0)
+            {
+                Y = 0;
+            }
+
+            if (Y > g.Height)
+            {
+                Y = g.Height;
+            }
+
+
+            O.Margin = new Thickness(X - R / 2, Y - R / 2, 0, 0);
+        }
+
+        public void Up(double dy)
+        {
+            Y -= dy;
+
+            Draw();
+        }
+
+        public void Right(double dx)
+        {
+            X += dx;
+
+            Draw();
+        }
+
+        public void Jump()
+        {
+            Random rnd = new Random();
+
+            X = rnd.NextDouble() * g.Width;
+            Y = rnd.NextDouble() * g.Height;
+
+            Draw();
+        }
+
+        public void ToHome()
+        {
+            X = g.Width / 2;
+            Y = g.Height / 2;
+
+            Draw();
+        }
+    }
+}
